f28511b7c4eb5cb55e097b24789639a77482c3b3
andres050588 <andres050588@gmail.com>
2025-07-06 15:40:19 +0200

    redis connection da ipv4 a ipv6 con ?family=0`

--- user_service/src/redisClient.js
+++ user_service/src/redisClient.js
@@ -1,13 +1,13 @@
 import Redis from "ioredis"
 
-const redis = new Redis(process.env.REDIS_URL, {
-    tls: {},
+const redis = new Redis(`${process.env.REDIS_URL}?family=0`, {
+    tls: {}, // Railway richiede TLS, anche se non lo dice esplicitamente nel nome host
+    connectTimeout: 5000,
     retryStrategy(times) {
         const delay = Math.min(times * 50, 2000)
         console.log(`Tentativo riconnessione Redis Nr${times} in ${delay}ms`)
         return delay
-    },
-    connectTimeout: 5000
+    }
 })
 
 redis.on("connect", () => {
