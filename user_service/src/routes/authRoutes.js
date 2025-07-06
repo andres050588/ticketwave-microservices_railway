08fe1b6b09ff001f6ba2470309298e11d681df4c
andres050588 <andres050588@gmail.com>
2025-07-06 23:15:23 +0200

    inserito userList byAdmin e deleteUser_ByAdmin

--- user_service/src/routes/authRoutes.js
+++ user_service/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, register, updateUserProfile, userProfile, userProfile_ByAdmin, usersList_ByAdmin } from "../controllers/authController.js"
+import { deleteUser_ByAdmin, login, register, updateUserProfile, userProfile, userProfile_ByAdmin, usersList_ByAdmin } from "../controllers/authController.js"
 import { verifyToken, verifyAdmin } from "../middleware/verifyToken.js"
 import "../events/subscriber.js"
 
@@ -13,4 +13,6 @@ routerAuth.put("/users/:id", verifyToken, updateUserProfile)
 //Admin routes
 routerAuth.get("/admin/users", verifyToken, verifyAdmin, usersList_ByAdmin)
 routerAuth.get("/admin/users/:id", verifyToken, verifyAdmin, userProfile_ByAdmin)
+routerAuth.delete("/admin/users/:id", verifyToken, verifyAdmin, deleteUser_ByAdmin)
+
 export default routerAuth
